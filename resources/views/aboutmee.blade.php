eb5846a582cebc243c4ee751231c4d0b2c39f728
afsoms <57783718+afsoms@users.noreply.github.com>
2019-11-16 00:26:15 +0700

    Add files via upload

---
+++ resources/views/aboutmee.blade.php
@@ -0,0 +1,109 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <title>about mee</title>
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+<!-- Styles -->
+<style>
+    html, body {
+        background:url(https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-1.2.1&w=1000&q=80);
+        color: #636b6f;
+        font-family: 'Nunito', sans-serif;
+        font-weight: 200;
+        height: 100vh;
+        margin: 0 auto;
+        background-size: cover; 
+    }
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+html {
+    font-size: 100%;
+}
+
+body {
+    font-family: 'Open Sans', sans-serif;
+    color: rgb(230, 245, 24);
+    line-height: 1.6;
+}
+
+h1 {
+    text-align: center;
+    font-family: "Comic Sans MS", cursive, sans-serif;
+    font-size: 4rem;
+    font-weight: normal;
+    line-height: 5.5rem;
+}
+
+h3 {
+    font-size: 1.35rem;
+    line-height: 2rem;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+}
+
+.containers {
+    margin: 0 auto;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: url(https://wallpaper.campus-star.com/app/uploads/2019/09/green-wall-10.jpg);
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover; 
+}
+.contts {
+    margin: 0 auto;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: url(https://www.w3schools.com/html/img_girl.jpg);
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover; 
+}
+
+
+.items {
+    padding: 0 30px 0 30px;
+    text-align: center;
+}
+</style>
+</head>
+
+<body>
+    <h1 style="font-family:impact;color:black;text-align: center;"><b>About me</b></h1>
+    
+  <div class="contts">
+     
+   
+      <div class="item">
+          <br><img src="https://scontent.fbkk5-4.fna.fbcdn.net/v/t1.0-9/56542029_2094214890696712_244314404269588480_n.jpg?_nc_cat=103&_nc_oc=AQk2D8nbVrjCHlmvmdCk9oD3iaNmm7qc8eafNVEaaoBNGTlOfqqVrbBMqlunUJQlseskIGlU29_55BsPBoFX1SUP&_nc_ht=scontent.fbkk5-4.fna&oh=87e27053f8602d0c3da05f9a29392385&oe=5E59C563" alt="Girl in a jacket" style="width:300px;height:300px;"></br>
+         
+          <h3 style="font-family:comic sans ms;color:white"><br>My name is Affan Maseng <p>I am studying at Fatoni university<p></p>
+            Now i am 22 years old</p></h3>
+          
+            
+      </div>
+  </div>
+
+  
+
+
+
+</body>
+
+</html>
