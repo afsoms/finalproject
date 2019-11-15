08b498e3fb7618240d2bb8a5fdf7eb3b807ee031
afsoms <57783718+afsoms@users.noreply.github.com>
2019-11-16 00:19:24 +0700

    Add files via upload

---
+++ storage/framework/views/57e9ef8e4e198984b4359f1a5b378b501605d701.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background:url(https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-1.2.1&w=1000&q=80);
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0 auto;
+                background-size: cover; 
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+               
+            }
+
+            .links > a {
+                color:white;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+           
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <?php if(Route::has('login')): ?>
+                <div class="top-right links">
+                    <?php if(auth()->guard()->check()): ?>
+                        <a href="<?php echo e(url('/home')); ?>">Home</a>
+                    <?php else: ?>
+                        <a href="<?php echo e(route('login')); ?>">Login</a>
+
+                        <?php if(Route::has('register')): ?>
+                            <a href="<?php echo e(route('register')); ?>">Register</a>
+                        <?php endif; ?>
+                    <?php endif; ?>
+                </div>
+            <?php endif; ?>
+         
+            <div class="content">
+                <h2 style="font-family:Nunito;color:white">wellcome to </h2>
+                 <div class="title m-b-md">
+                <p style="font-family:kg defying gravity;color:white;">  Afsoms side</p>
+               
+                </div>
+
+                <div class="links">
+                    <a href="https://infallible-agnesi-4b1613.netlify.com/">about me</a>
+                    <a href="https://www.facebook.com/affan.maseng.5">facebook</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+<?php /**PATH C:\xampp\htdocs\laravel\affan\resources\views/welcome.blade.php ENDPATH**/ ?>
